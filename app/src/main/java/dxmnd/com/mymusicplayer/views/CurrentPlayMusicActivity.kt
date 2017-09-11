aad78a1dcc59f1693cd0334d500dbd7c1487568e
KimHunJin <ysg01129@naver.com>
2017-09-12 01:12:16 +0900

    [Complete] 1. main activity layout
    [To Do]
    1. Current Play Music Activity & Lyaout
    2. Service
    3. Client - Server communication

---
+++ app/src/main/java/dxmnd/com/mymusicplayer/views/CurrentPlayMusicActivity.kt
@@ -0,0 +1,14 @@
+package dxmnd.com.mymusicplayer.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import dxmnd.com.mymusicplayer.R
+
+class CurrentPlayMusicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_play_music)
+    }
+}
